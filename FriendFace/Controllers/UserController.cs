3b0b09bcd3f289793bc09518a06a6e1b76fb7937
pjpedersen <116650892+pjpedersen@users.noreply.github.com>
2023-12-18 00:04:18 +0100

    modified editprofile to partial view, added js to send ajax, js to set success message & timer, js to validate ajax form and controller logic to ignore model binding from framework

--- FriendFace/Controllers/UserController.cs
+++ FriendFace/Controllers/UserController.cs
@@ -66,16 +66,29 @@ namespace FriendFace.Controllers
 
             // Optionally, you can pre-populate the model with user data if needed.
 
-            return View("EditProfile", model);
+            return View("_EditProfile", model);
         }
 
 
         [HttpPost("User/Profile/SaveEditProfile")]
-        public async Task<IActionResult> SaveEditProfile(UserProfileViewModel model)
+        public async Task<IActionResult> SaveEditProfile([FromForm]UserProfileViewModel model)
         {
+
+            var fieldsToIgnore = new List<string> {
+                "ChangePasswordViewModel", "user.Likes", "user.Posts", "user.Comments",
+                "user.LastName", "user.FirstName", "user.Followers", "user.Following"
+            };
+
+            foreach (var field in fieldsToIgnore)
+            {
+                ModelState.Remove(field);
+            }
+
+            _logger.LogInformation("SaveEditProfile Called");
             var user = await _userManager.GetUserAsync(User); // Retrieve the current user
             if (user == null)
             {
+                _logger.LogWarning("User is null");
                 // Handle the case where the user is not found
                 return NotFound(); // or another appropriate response
             }
@@ -84,7 +97,6 @@ namespace FriendFace.Controllers
             if (!string.IsNullOrWhiteSpace(model.user.Email) && model.user.Email != user.Email)
             {
                 user.Email = model.user.Email;
-                // You may also want to handle email confirmation logic here
             }
 
             // Update other fields similarly...
@@ -99,20 +111,20 @@ namespace FriendFace.Controllers
                 if (string.IsNullOrWhiteSpace(model.ChangePasswordViewModel.CurrentPassword))
                 {
                     ModelState.AddModelError("ChangePasswordViewModel.CurrentPassword", "Current password is required.");
-                    return View("EditProfile", model);
+                    return View("_EditProfile", model);
                 }
 
                 var checkPassword = await _userManager.CheckPasswordAsync(user, model.ChangePasswordViewModel.CurrentPassword);
                 if (!checkPassword)
                 {
                     ModelState.AddModelError("ChangePasswordViewModel.CurrentPassword", "Current password is not correct.");
-                    return View("EditProfile", model);
+                    return View("_EditProfile", model);
                 }
 
                 if (model.ChangePasswordViewModel.NewPassword != model.ChangePasswordViewModel.ConfirmNewPassword)
                 {
                     ModelState.AddModelError("ChangePasswordViewModel.ConfirmNewPassword", "The new password and confirmation password do not match.");
-                    return View("EditProfile", model);
+                    return View("_EditProfile", model);
                 }
 
                 var changePasswordResult = await _userManager.ChangePasswordAsync(user, model.ChangePasswordViewModel.CurrentPassword, model.ChangePasswordViewModel.NewPassword);
@@ -122,7 +134,7 @@ namespace FriendFace.Controllers
                     {
                         ModelState.AddModelError(string.Empty, error.Description);
                     }
-                    return View("EditProfile", model);
+                    return View("_EditProfile", model);
                 }
             }
 
@@ -136,8 +148,33 @@ namespace FriendFace.Controllers
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
-                return View("EditProfile", model);
             }
+
+            if (HttpContext.Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                if (!ModelState.IsValid)
+{
+    foreach (var state in ModelState)
+    {
+        foreach (var error in state.Value.Errors)
+        {
+            _logger.LogWarning("Error in {0}: {1}", state.Key, error.ErrorMessage);
+        }
+    }
+}
+
+                if (!ModelState.IsValid || !updateResult.Succeeded)
+                {
+                    _logger.LogWarning("Modelstate is invalid");
+                    // If ModelState is not valid, or user update failed, return the errors in JSON format
+                    var errors = ModelState.Values.SelectMany(v => v.Errors.Select(b => b.ErrorMessage));
+                    return Json(new { success = false, errors = errors });
+                }
+
+                // If everything is fine for AJAX request, return a success message
+                return Json(new { success = true, message = "Profile updated successfully!" });
+            }
+
 
             // Redirect or return a view upon successful update
             return RedirectToAction("Profile", "User", new {userId = _userQueryService.GetLoggedInUser().Id }); // or another appropriate view
